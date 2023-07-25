46576bf319a270dea4571d670862b50962098196
hai.chi.nguyen <chihai7506@gamil.com>
2023-07-25 13:49:34 +0700

    update core

---
+++ Selenium/src/main/java/core/driver/manager/Base/BaseDriver.java
@@ -0,0 +1,59 @@
+package core.driver.manager.Base;
+
+import core.driver.manager.setting.DriverProperty;
+import org.openqa.selenium.WebDriver;
+
+
+/**
+ * Create 'abstract' type specified basedriver 'BaseDriver' containing abstract
+ * methods for driver
+ */
+public abstract class BaseDriver {
+
+	/**
+	 * a remote control, an interface that permits to execute tests over browsers
+	 */
+	protected WebDriver webDriver;
+
+	/**
+	 * Driver's properties
+	 */
+	protected DriverProperty driverProperty;
+
+	/**
+	 * Get web driver control
+	 * 
+	 * @return web driver
+	 */
+	public WebDriver getWebDriver() {
+		return this.webDriver;
+	}
+
+	/**
+	 * Get driver's properties setting
+	 * 
+	 * @return driver's properties (ex: name, value,...)
+	 */
+	public DriverProperty getDriverProperty() {
+		return this.driverProperty;
+	}
+
+	/**
+	 * Initializes driver with given driver properties
+	 * 
+	 * @param property - driver's properties setting
+	 */
+	public BaseDriver(DriverProperty property) {
+		this.driverProperty = property;
+	}
+
+	/**
+	 * Initializes driver to run a local web driver
+	 */
+	public abstract void createLocalDriver();
+
+	/**
+	 * Initializes driver to run a remote web driver
+	 */
+	public abstract void createRemoteDriver();
+}
---
+++ Selenium/src/main/java/utils/helper/BrowserSettingHelper.java
@@ -0,0 +1,33 @@
+package utils.helper;
+
+import core.driver.manager.setting.DriverProperty;
+import org.json.simple.JSONObject;
+
+import com.google.gson.Gson;
+
+/**
+ * <p>
+ * A Collection of helper methods for browser setting.
+ * </p>
+ */
+public class BrowserSettingHelper {
+
+	/**
+	 * This method will parse browser setting value json to DriverProperty
+	 * 
+	 * @param file        - a string path of file
+	 * @param sectionName - a string of key driver config
+	 * @return - Driver property
+	 */
+	public static DriverProperty getDriverProperty(String file, String sectionName) {
+		try {
+			JSONObject jObject = JsonHelper.getJSONObject(file);
+			DriverProperty property = new Gson().fromJson(jObject.get(sectionName).toString(), DriverProperty.class);
+
+			return property;
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+}
