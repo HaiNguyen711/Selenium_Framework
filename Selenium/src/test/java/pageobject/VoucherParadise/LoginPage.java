de41bdc745a5c534764ba232c4f76f6b2b60115f
QuyetNguyenTran <138967649+QuyetNguyenTran@users.noreply.github.com>
2023-08-11 17:42:55 +0700

    implement 2nd test case
    implement 2nd test case

--- Selenium/src/test/java/pageobject/VoucherParadise/LoginPage.java
+++ Selenium/src/test/java/pageobject/VoucherParadise/LoginPage.java
@@ -9,26 +9,45 @@ import utils.constant.Constant;
 import utils.helper.LocatorHelper;
 
 public class LoginPage extends BasePage {
-	
+
 	LocatorHelper locator = new LocatorHelper(Constant.LOCATOR_PATH, LoginPage.class);
-	
+
 	TextBox txtEmail = locator.getLocator(ControlType.TEXTBOX, "txtEmail");
 	TextBox txtPassword = locator.getLocator(ControlType.TEXTBOX, "txtPassword");
 	Button btnLogin = locator.getLocator(ControlType.BUTTON, "btnLogin");
 	Label lblLogin = locator.getLocator(ControlType.LABEL, "lblLogin");
-	
+	Label lblNotificationMessage = locator.getLocator(ControlType.LABEL, "lblNotificationMessage");
+	Label lblNotificationDescription = locator.getLocator(ControlType.LABEL, "lblNotificationDescription");
 
 	public HomePage login(Account account) {
-		
+
 		txtEmail.enter(account.getEmail());
 		txtPassword.enter(account.getPassword());
 		btnLogin.click();
 		return new HomePage();
 	}
-	
+
+	public LoginPage failureLogin(Account account) {
+
+		txtEmail.enter(account.getEmail());
+		txtPassword.enter(account.getPassword());
+		btnLogin.click();
+		return this;
+	}
+
 	public LoginPage waitForLoginPageIsDisplayed() {
 		lblLogin.waitForVisibility();
 		return this;
+	}
+
+	public String getNotificationMessage() {
+		String acc = lblNotificationMessage.getText();
+		
+		return lblNotificationMessage.getText();
+	}
+
+	public String getNotificationDescription() {
+		return lblNotificationDescription.getText();
 	}
 
 }
--- Selenium/src/test/java/tests/VoucherParadise/LoginTest.java
+++ Selenium/src/test/java/tests/VoucherParadise/LoginTest.java
@@ -20,7 +20,7 @@ public class LoginTest extends BaseTest {
 	 * Test id: LO01
 	 * Verify that user will be able to login with a valid username and valid password.
 	 * 
-	 * @author 
+	 * @author quyet.nguyen
 	 */
 	@Test
 	public void LO01() {
@@ -28,7 +28,6 @@ public class LoginTest extends BaseTest {
 		Environment environment = new Environment();
 		AssertHelper assertHelper = new AssertHelper();
 		LoginPage loginPage = new LoginPage();
-		PartnersPage pa;
 		HomePage homePage;
 		String sWelcomTitle = environment.getValue("welcomTitle");
 		
@@ -43,5 +42,34 @@ public class LoginTest extends BaseTest {
 		
 		Log.STEP("4.Logout");
 		loginPage = homePage.logout();
+	}
+	
+	/**
+	 * Test id: LO02
+	 * Verify that user cannot login with a valid username and an invalid password.
+	 * 
+	 * @author quyet.nguyen
+	 */
+	@Test
+	public void LO02() {
+		
+		Environment environment = new Environment();
+		AssertHelper assertHelper = new AssertHelper();
+		LoginPage loginPage = new LoginPage();
+		String sInvalidPassword = environment.getValue("invalidPassword");
+		String sNotificationMessage = environment.getValue("notificationMessage");
+		String sNotificationDescription = environment.getValue("notificationDescription");
+		
+		Log.STEP("1.Navigate to Voucher Paradise Admin Portal website");
+		Log.STEP("2.Enter valid username and invalid password");
+		Log.STEP("3.Click on LogIn button");
+		Account acc = AccountHepler.getUser("Admin");
+		acc.setPassword(sInvalidPassword);
+		loginPage = loginPage.failureLogin(acc);
+		
+		Log.verify("Failure login");
+		assertHelper.assertEquals(loginPage.getNotificationMessage(),sNotificationMessage, "Failure login");
+		assertHelper.assertEquals(loginPage.getNotificationDescription(),sNotificationDescription, "Failure login");
+		
 	}
 }
