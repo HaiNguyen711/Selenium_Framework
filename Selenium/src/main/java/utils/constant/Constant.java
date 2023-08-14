2d9a279d3507a601152c585fff9771c3c5ae2501
HaiNguyen711 <chihai7506@gmail.com>
2023-08-14 11:25:11 +0000

    Haiupdate CICD

--- Selenium/src/main/java/utils/constant/Constant.java
+++ Selenium/src/main/java/utils/constant/Constant.java
@@ -9,13 +9,13 @@ import utils.helper.Utilities;
 
 public class Constant {
 	public static final String VOUCHER_PARADISE_URL = "http://18.232.153.220:10000/";
-	public static final String HUB_URL = JsonHelper.getValue(Utilities.getProjectPath() + "\\src\\test\\resources\\grid\\node_config.json", "hub") + "/wd/hub";
-	public static final String DRIVER_SETTING_FILE = Utilities.getProjectPath() + "\\src\\test\\resources\\driver.setting.properties.json";
-	public static final String DATA_URL = Utilities.getProjectPath() + "\\src\\test\\resources";
-	public static final String USER_DATA = DATA_URL + "\\data\\user\\Users.json";
-	public static final String VARIABLE_DATA = DATA_URL + "\\data\\environment\\";
-	public static final String LOCATOR_PATH = DATA_URL + "\\locator\\";
-	public static final String IMAGE_DATA = DATA_URL + "\\data\\images";
+	public static final String HUB_URL = JsonHelper.getValue(Utilities.getProjectPath() + "/src/test/resources/grid/node_config.json", "hub") + "/wd/hub";
+	public static final String DRIVER_SETTING_FILE = Utilities.getProjectPath() + "/src/test/resources/driver.setting.properties.json";
+	public static final String DATA_URL = Utilities.getProjectPath() + "/src/test/resources";
+	public static final String USER_DATA = DATA_URL + "/data/user/Users.json";
+	public static final String VARIABLE_DATA = DATA_URL + "/data/environment/";
+	public static final String LOCATOR_PATH = DATA_URL + "/locator/";
+	public static final String IMAGE_DATA = DATA_URL + "/data/images";
 	
 	public static final String NULL = "null";
 	
